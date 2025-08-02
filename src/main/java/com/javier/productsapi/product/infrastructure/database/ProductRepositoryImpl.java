42d8551be105c4afcdb9e43f22cb2022ff509e06
javi <javiermengual@live.com>
2025-08-02 12:21:52 +0200

    feat: integrate ProductEntityMapper in ProductRepositoryImpl for improved data handling

--- src/main/java/com/javier/productsapi/product/infrastructure/database/ProductRepositoryImpl.java
+++ src/main/java/com/javier/productsapi/product/infrastructure/database/ProductRepositoryImpl.java
@@ -3,6 +3,8 @@ package com.javier.productsapi.product.infrastructure.database;
 import com.javier.productsapi.product.domain.Product;
 import com.javier.productsapi.product.domain.ProductRepository;
 import com.javier.productsapi.product.infrastructure.database.entity.ProductEntity;
+import com.javier.productsapi.product.infrastructure.database.mapper.ProductEntityMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -10,28 +12,33 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class ProductRepositoryImpl implements ProductRepository {
 
-    private final List<ProductEntity> products;
+    private final List<ProductEntity> products = new ArrayList<>();
 
-    public ProductRepositoryImpl() {
-        this.products = new ArrayList<>();
-    }
+    private final ProductEntityMapper productEntityMapper;
+
 
     @Override
     public void upsert(Product product) { // post y update a la vez
-        deleteById(product.getId()); // elimina si existe
-        products.add(product);
+        ProductEntity productEntity = productEntityMapper.mapToProductEntity(product);
+
+        products.add(productEntity);
     }
 
     @Override
     public Optional<Product> findById(Long id) {
-        return products.stream().filter(product -> product.getId().equals(id)).findFirst();
+        return products.stream().filter(product -> product.getId().equals(id))
+                .findFirst()
+                .map(productEntityMapper::mapToProduct);
     }
 
     @Override
     public List<Product> findAll() {
-        return products;
+        return products.stream()
+                .map(productEntityMapper::mapToProduct)
+                .toList();
     }
 
     @Override
