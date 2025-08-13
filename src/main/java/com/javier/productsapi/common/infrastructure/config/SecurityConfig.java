c5e9b3daab21769f3a93dbc618a5f4a911b55ad1
javi <javiermengual@live.com>
2025-08-13 18:19:57 +0200

    feat(security): Configure Spring Security to permit access to product API endpoints

--- src/main/java/com/javier/productsapi/common/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/javier/productsapi/common/infrastructure/config/SecurityConfig.java
@@ -2,15 +2,35 @@ package com.javier.productsapi.common.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers(
+                                "/api/v1/products/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+
 
     @Bean
     public UserDetailsService userDetailsService() {
