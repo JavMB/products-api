c3b8a6d9d1880c1042155a9c3bcb4ae5769202b6
javi <javiermengual@live.com>
2025-08-13 18:08:01 +0200

    feat(security): Implement in-memory user details service with default users

--- src/main/java/com/javier/productsapi/common/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/javier/productsapi/common/infrastructure/config/SecurityConfig.java
@@ -1,11 +1,31 @@
 package com.javier.productsapi.common.infrastructure.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN", "USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 
 
 }
