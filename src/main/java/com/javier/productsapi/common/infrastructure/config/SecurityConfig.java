93f86ba62609cc0eb1ec2c64541ce6b4f0cdf6ea
javi <javiermengual@live.com>
2025-08-13 15:37:29 +0200

    feat(security): Add Spring Security dependencies and configuration class

---
+++ src/main/java/com/javier/productsapi/common/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.javier.productsapi.common.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+
+}
