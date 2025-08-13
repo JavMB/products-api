b52459dce2611c8cae99874e015e2b6e615951de
javi <javiermengual@live.com>
2025-08-13 17:26:32 +0200

    .

--- src/main/java/com/javier/productsapi/ProductsApiApplication.java
+++ src/main/java/com/javier/productsapi/ProductsApiApplication.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.FilterType;
 })
 
 public class ProductsApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProductsApiApplication.class, args);
 
