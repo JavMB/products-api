ca6851118781abad016318d384a0b8874b27f63f
javi <javiermengual@live.com>
2025-08-18 12:37:11 +0200

    feat(security): Add JWT authentication support with token generation

---
+++ src/main/java/com/javier/productsapi/common/infrastructure/service/JwtService.java
@@ -0,0 +1,58 @@
+package com.javier.productsapi.common.infrastructure.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    // @Value("${jwt.secret}") //desarollo solo sino ENV
+    private static final String SECRET_KEY = "QWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo0NTY3ODkwMTIzNDU2Nzg5MDEyMw==";
+    private static final long TOKEN_EXPIRATION = 1000 * 60 * 60 * 24; // 1 dia
+
+
+    public String generateToken(UserDetails userDetails) {
+
+        Map<String, Object> claims = Map.of("authorities", userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toArray(String[]::new));
+
+        return generateToken(claims, userDetails.getUsername());
+
+
+    }
+
+
+    public String generateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + TOKEN_EXPIRATION))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact()
+                ;
+
+    }
+
+    private Key getSignKey() {
+
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+}
