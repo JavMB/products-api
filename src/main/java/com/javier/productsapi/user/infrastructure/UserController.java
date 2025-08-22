a718d3be33bd010f5ba828a8c642771f37986140
javi <javiermengual@live.com>
2025-08-22 11:59:27 +0200

    feat(auth): Implement login functionality with JWT token generation

---
+++ src/main/java/com/javier/productsapi/user/infrastructure/UserController.java
@@ -0,0 +1,45 @@
+package com.javier.productsapi.user.infrastructure;
+
+import com.javier.productsapi.common.infrastructure.service.JwtService;
+import com.javier.productsapi.user.infrastructure.dto.LoginRequestDto;
+import com.javier.productsapi.user.infrastructure.dto.LoginResponseDto;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@Tag(name = "User", description = "User API operations")
+@RequiredArgsConstructor
+@Slf4j
+public class UserController {
+
+    private final JwtService jwtService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> getUserById(LoginRequestDto loginRequestDto) {
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(loginRequestDto.getEmail())
+                .password(loginRequestDto.getPassword())
+                .roles("USER")
+                .build();
+
+        String token = jwtService.generateToken(user);
+
+        LoginResponseDto loginResponseDto = new LoginResponseDto();
+        loginResponseDto.setToken(token);
+
+        return ResponseEntity.ok(new LoginResponseDto());
+
+
+    }
+
+
+}
