e2a950ae5829104f5a5234d037a4c09a7452a0cb
Javier Mengual <javiermengual@live.com>
2025-08-23 13:09:28 +0200

    feat(auth): Enhance JWT filter to validate authorization header and log token

--- src/main/java/com/javier/productsapi/common/infrastructure/filters/JwtFilter.java
+++ src/main/java/com/javier/productsapi/common/infrastructure/filters/JwtFilter.java
@@ -4,6 +4,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -18,11 +20,17 @@ public class JwtFilter extends OncePerRequestFilter {
 
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-        
+        String authorization = request.getHeader("Authorization");
 
+        if (authorization == null && !authorization.startsWith("Bearer ")) {
+            log.error("Authorization header not found");
+            filterChain.doFilter(request, response);
+            return;
+        }
 
-
+        String token = authorization.substring(7);
+        log.info("token: {}", token);
     }
 }
